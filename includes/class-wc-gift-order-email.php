7cc0bd96476057ef98ef1f90d5e7cabf69939648
Aidan G <awgraf@wisc.edu>
2021-02-02 22:37:40 -0600

    Changed email validation to a built in PHP filter

--- includes/class-wc-gift-order-email.php
+++ includes/class-wc-gift-order-email.php
@@ -42,61 +42,6 @@ class WC_Gift_Order_Email extends WC_Email {
 		// Call parent constructor to load any other defaults not explicity defined here
 		parent::__construct();
 	}
-	
-	/**
-	Validate an email address.
-	Provide email address (raw input)
-	Returns true if the email address has the email 
-	address format and the domain exists.
-	Function taken from the following website:
-	https://www.linuxjournal.com/article/9585
-	*/
-	public function validEmail($email) {
-	   $isValid = true;
-	   $atIndex = strrpos($email, "@");
-	   if (is_bool($atIndex) && !$atIndex) {
-	      $isValid = false;
-	   }
-	   else {
-	      $domain = substr($email, $atIndex+1);
-	      $local = substr($email, 0, $atIndex);
-	      $localLen = strlen($local);
-	      $domainLen = strlen($domain);
-	      if ($localLen < 1 || $localLen > 64) {
-		 // local part length exceeded
-		 $isValid = false;
-	      } else if ($domainLen < 1 || $domainLen > 255) {
-		 // domain part length exceeded
-		 $isValid = false;
-	      } else if ($local[0] == '.' || $local[$localLen-1] == '.') {
-		 // local part starts or ends with '.'
-		 $isValid = false;
-	      } else if (preg_match('/\\.\\./', $local)) {
-		 // local part has two consecutive dots
-		 $isValid = false;
-	      } else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
-		 // character not valid in domain part
-		 $isValid = false;
-	      } else if (preg_match('/\\.\\./', $domain)) {
-		 // domain part has two consecutive dots
-		 $isValid = false;
-	      } else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
-			 str_replace("\\\\","",$local))) {
-		 // character not valid in local part unless 
-		 // local part is quoted
-		 if (!preg_match('/^"(\\\\"|[^"])+"$/',
-		     str_replace("\\\\","",$local))) {
-		    $isValid = false;
-		 }
-	      }
-// Not including DNS check currently
-// 	      if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A"))) {
-// 		 // domain not found in DNS
-// 		 $isValid = false;
-// 	      }
-	   }
-	   return $isValid;
-	}
 
 	/**
 	 * Determine if the email should actually be sent and setup email merge variables
@@ -124,7 +69,8 @@ class WC_Gift_Order_Email extends WC_Email {
 		if ( empty($email) ) {
 			return;
 		} else {
-			if ( !(validEmail($email)) ) {
+			$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+			if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
 				return;
 			}
 		}
